b65531fb23c51afdfb44f51eb092268c6f07ff95
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-10-01 21:56:48 +0100

    Fix scroll bounce navbar flicker with buffer in js

--- js/main.js
+++ js/main.js
@@ -2,18 +2,26 @@
 
 console.log("Connected");
 
+let  isMinimized = false;
+
 window.addEventListener('scroll', function() {
     const headerContainer = document.querySelector('.header-container');
     const h4Element = document.querySelector('.logo h4');
     const logo = document.querySelector('.logo');
 
-    if (window.scrollY > 50) { // Change value based on when you want the minimization to occur
+    /* A scroll bounce issue feedback loop  occurs then scroll wheel is position at the threshold the nav
+    minize cause a flickering effect */
+    const revertThresholdBuffer =  isMinimized ? 80 : 100;
+
+    if (window.scrollY > revertThresholdBuffer) { // Change value based on when you want the minimization to occur
         headerContainer.classList.add('minimized');
         logo.classList.add('minimized');
         if (h4Element) h4Element.style.display = 'none'; // hide the h4
+        isMinimized = true;
     } else {
         headerContainer.classList.remove('minimized');
         logo.classList.remove('minimized');
-        if (h4Element) h4Element.style.display = 'block'; // show the h4    
+        if (h4Element) h4Element.style.display = 'block'; // show the h4
+        isMinimized = false;    
     }
 });
