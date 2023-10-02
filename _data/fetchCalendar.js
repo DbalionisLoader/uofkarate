1a35da21054cb3ceda76019ed0308d4f397f68c6
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-10-02 20:18:55 +0100

    Add console log to fetchcalendar to error check

--- _data/fetchCalendar.js
+++ _data/fetchCalendar.js
@@ -13,12 +13,14 @@ const {google} = require('googleapis'); /* Required for google object import */
 const {auth} = require('google-auth-library'); /* Required for auth object */
 const fs = require('fs'); /* node.js file system to read json and write to json*/
 const axios = require('axios'); /* Promise bases ? HTTP client */
+const { Console } = require('console');
 require('dotenv').config(); 
 
 //Create creadiant object 
 /*  const crediantials = JSON.parse(fs.readFileSync('uol-karate-events-513d09d09e24.json','utf8'));  */
  const credentials = JSON.parse(process.env.CREDS); 
-
+ console.log("display crediantials");
+ console.log(credentials);
 
 
 async function fetchGoogleCalenderEvents() {
@@ -37,6 +39,7 @@ async function fetchGoogleCalenderEvents() {
         timeMin: (new Date()).toISOString(), //get events from current time
     });
     //Check event object to retrieve correct json tag names
+    console.log("display retrieved item");
     console.log(response.data.items[0]);
     //Map through the events list object - extract required fields and write them into a json file
     const events = response.data.items.map(event => {
@@ -48,7 +51,8 @@ async function fetchGoogleCalenderEvents() {
          
         }; 
     });
-
+    console.log("Display errors");
+    console.log(events);
     fs.writeFileSync('_data/calendarEvents.json', JSON.stringify(events));
 }
 
