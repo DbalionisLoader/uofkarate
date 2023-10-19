dacbe9df386626b7dec2666316b4a76e3b0f8490
Dovydas B <27741806+DbalionisLoader@users.noreply.github.com>
2023-10-19 19:30:14 +0100

    Add metadata.js and meta to head

---
+++ _data/metadata.js
@@ -0,0 +1,11 @@
+module.exports = {
+	title: "University of Liverpool - Karate Sport Club ",
+	url: "",
+	language: "en",
+	description: "Achieve Balance and Confidence with University of Liverpool Karate Club! Join our dynamic community of martial artists, learn self-defense, and unlock your potential. Explore our classes, events, and more!",
+	author: {
+		name: "Dovydas Balionis",
+		email: "dbalionis@outlook.com",
+		url: "https://dovydasbalionis.co.uk/"
+	}
+}
